3e5a6e81610e997cdb61516d28d49d82d1204139
Isimis <132483563+Isimis@users.noreply.github.com>
2023-09-06 11:01:28 +0200

    Adding comments

--- Maze_Generator.cpp
+++ Maze_Generator.cpp
@@ -1,20 +1,24 @@
-#include <iostream>
-#include <vector>
-#include <random>
-#include <algorithm>
+#include <iostream> // output
+#include <vector> // vector
+#include <random> // rng
+#include <algorithm> // shuffle
 
 using namespace std;
 
+// Wielkość labiryntu - będzie działać poprawnie przy wszystkich liczbach nieparzystych
 const int width = 29;
 const int height = 29;
 
+// Tworzenie labiryntu
 char grid[height][width];
 
-random_device rd;
-mt19937 gen(rd());
-uniform_int_distribution<int> dist(0, 3);
+// Tworzenie rng
+random_device rd; // Źródło losowości
+mt19937 gen(rd()); // Wdrożenie algorytmu mt19937
+uniform_int_distribution<int> dist(0, 3); // Losuje liczby od 0 do 3 co posłuży do losowania kierunków
 
-void initGrid() {
+// Zapełnienie labiryntu ścianami
+void InitGrid() {
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
             grid[i][j] = '#';
@@ -22,31 +26,36 @@ void initGrid() {
     }
 }
 
-bool isValid(int x, int y) {
+// Sprawdzanie czy pole, które ma zostać zburzona ściana znajduje się w labiryncie
+bool IsValid(int x, int y) {
     return x > 0 && x < height && y > 0 && y < width;
 }
 
-void recursiveBacktracking(int x, int y) {
+// Zastosowanie metody Recursive Backtracking do generowanie ścieżek labiryntu
+void RecursiveBacktracking(int x, int y) {
     grid[x][y] = ' ';
 
-    int directions[4][2] = {{0, -2}, {0, 2}, {-2, 0}, {2, 0}};
+    int directions[4][2] = {{0, -2}, {0, 2}, {-2, 0}, {2, 0}}; // Wyznaczenie kierunków ruchu generatora
     vector<int> dirs = {0, 1, 2, 3};
-    shuffle(dirs.begin(), dirs.end(), gen);
+    shuffle(dirs.begin(), dirs.end(), gen); // Losowanie kierunków
 
+    // Wyznaczanie ścieżki dla każdego kierunku
     for (int i = 0; i < 4; i++) {
         int dir = dirs[i];
         int newX = x + directions[dir][0];
         int newY = y + directions[dir][1];
 
-        if (isValid(newX, newY) && grid[newX][newY] == '#') {
-            grid[newX][newY] = ' ';
+        // Generowanie ścieżki
+        if (IsValid(newX, newY) && grid[newX][newY] == '#') {
+            grid[newX][newY] = ' '; 
             grid[x + directions[dir][0] / 2][y + directions[dir][1] / 2] = ' ';
-            recursiveBacktracking(newX, newY);
+            RecursiveBacktracking(newX, newY); // Wdrożenie rekursywności w celu generowania dalszej drogi
         }
     }
 }
 
-void printGrid() {
+// Rysowanie labiryntu
+void PrintGrid() {
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
             cout << grid[i][j] << " ";
@@ -55,22 +64,26 @@ void printGrid() {
     }
 }
 
+// Program
 int main() {
-    initGrid();
+    // Zapełnianie labiryntu
+    InitGrid();
 
+    // Losowanie i wyznaczanie początku ścieżki labiryntu
     vector<int> handX = {1, 3, 5, 7};
     vector<int> handY = {1, 3, 5, 7};
     shuffle(handX.begin(), handX.end(), gen);
     shuffle(handY.begin(), handY.end(), gen);
-
     int startX = handX[0];
     int startY = handY[0];
 
-    recursiveBacktracking(startX, startY);
+    // Generowanie ścieżki
+    RecursiveBacktracking(startX, startY); 
     grid[startX][startY] = 'S';
     grid[height - 2][width - 2] = 'E';
 
-    printGrid();
+    // Rysowanie labiryntu
+    PrintGrid();
 
     return 0;
 }
